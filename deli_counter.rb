a605778c169c9f21d7d56711fb8fb74a9120963a
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 10:01:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size > 0
+    baseString = "The line is currently: "1. Ada 2. Grace 3. Kent
+    katz_deli.each_with_index do |name, index|
+      puts baseString + (index + 1).to_s + ". " + name
+    end
+  else
+    puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, " + katz_deli[-1] +". You are number " + katz_deli.size.to_s +" in line."
+end
+
+def now_serving(katz_deli.pop)
+  puts "Currently serving" + katz_deli.pop
